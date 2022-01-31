afbf7721ef2689e2418b5d64c81fd94621c1e4a9
aymenbs22 <aymenbenslima1@gmail.com>
2022-01-31 13:21:30 -0800

    class Rectangle that inherits from BaseGeometry

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -17,4 +17,5 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__, self.__width, self.__height)
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                self.__width, self.__height)
