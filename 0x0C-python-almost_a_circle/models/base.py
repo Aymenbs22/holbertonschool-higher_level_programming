a7ce999c8884b1ea75832d869caff71055a0f8b0
Aymenbs22 <aymenbenslima1@gmail.com>
2022-02-07 14:52:28 -0800

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,3 +14,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """JSON string representation"""
+        if not list_dictionaries:
+            iist_dictionaries = []
+        else:
+            return json.dumps(list_dictionaries)
