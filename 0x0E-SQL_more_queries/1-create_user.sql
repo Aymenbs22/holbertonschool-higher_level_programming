aa2a686e594807e603fa80a9a58834b8bb864f02
Aymenbs22 <aymenbenslima1@gmail.com>
2022-03-09 04:24:51 -0800

    script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,2 @@
 -- script that creates the MySQL server user user_0d_1
-CREATE USER user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+ALTER USER user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
