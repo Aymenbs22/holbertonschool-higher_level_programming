79dcee38e7787ad3734412a2101caa084a7931ac
Aymenbs22 <aymenbenslima1@gmail.com>
2022-04-04 15:22:32 -0700

    script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const integer = process.argv[2];
 if (isNaN(integer)) {
-	console.log("Not a number");
+  console.log('Not a number');
 } else {
-	console.log("My number: " + parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
