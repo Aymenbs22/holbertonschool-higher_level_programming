57fa066dda3bae3e0d1296d190c2772ebc5ef7d9
Aymenbs22 <aymenbenslima1@gmail.com>
2022-04-09 05:40:58 -0700

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa"""
+
+
 import MySQLdb
 from sys import argv
-
 conn = MySQLdb.connect(host="localhost",
                        ort=3306,
                        user=argv[1],
