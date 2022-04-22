b753e2dbae10b580f1d31c6012f4e58eee69d809
Aymenbs22 <aymenbenslima1@gmail.com>
2022-04-22 15:24:28 -0700

    Python script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,8 @@
 import requests
 
 
-r = requests.get('https://intranet.hbtn.io/status')
-print("Body response:")
-print("     - type: {}".format(type(r.text)))
-print("     - content: {}".format(r.text))
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("    - type: {}".format(type(r.text)))
+    print("    - content: {}".format(r.text))
