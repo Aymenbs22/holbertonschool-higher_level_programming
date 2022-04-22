5247c381f1e49d3906e9f7297de7ca6cbd641501
Aymenbs22 <aymenbenslima1@gmail.com>
2022-04-22 16:17:29 +0000

    Python script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,3 +5,6 @@ import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
    html = response.read()
+print("- type: {}".format(type(html)))
+print("- content: {}".format(html))
+#print("- utf8 content: {}".format(html.encode('utf8')))
