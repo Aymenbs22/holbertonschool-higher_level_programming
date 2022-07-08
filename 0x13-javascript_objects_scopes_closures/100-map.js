c5ff8f1d3361f5ed2c4344f77cd42caef38311bb
Aymenbs22 <aymenbenslima1@gmail.com>
2022-07-08 13:12:57 +0100

    script that imports an array and computes a new array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-var imparray = require('./100-data.js').list;
-var i = 0;
+const imparray = require('./100-data.js').list;
+let i = 0;
 const map = imparray.map(l => l * i++);
 console.log(imparray);
 console.log(map);
